44d70ab0c334761fc9135e2c144ff61a4d7eed50
admin <admin@CSELAB9-60>
2020-03-12 15:10:08 +0530

    nigga

---
+++ src/sample/SamplePro.java
@@ -0,0 +1,13 @@
+package sample;
+import java.util.Scanner;
+
+public class SamplePro {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String s1,s2;
+		s1 = s.next();
+		System.out.println(s1);
+		
+	}
+
+}
